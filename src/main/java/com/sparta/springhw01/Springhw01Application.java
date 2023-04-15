a3162cca55d9d7a6cd2cdcd0d414ccd7767aba67
eivomin <eivomiin@gmail.com>
2023-04-15 09:18:46 +0900

    Initial commit

---
+++ src/main/java/com/sparta/springhw01/Springhw01Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springhw01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springhw01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springhw01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springhw01/Springhw01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springhw01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springhw01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
